41aafeda43fb18d6270be819c2601c485c017648
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-03 22:18:04 +0800

    Add sidebar design

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import Nav from "../../components/Nav";
+// import Nav from "../../components/Nav";
 import { Link } from "react-router-dom";
+import Sidebar from "../../components/Sidebar";
 
 const Dashboard = () => {
   return (
-    <>
-      <Nav />
-      <main className="flex flex-col items-center mt-36">
+    <div className="flex">
+      <Sidebar />
+      <main className="container px-4 pt-8 mx-auto">
         <p>This is dashboard</p>
         <Link to="/bookings">
           Click this to go to bookings {"("}This is supposed to be in the
           sidebar {")"}
         </Link>
       </main>
-    </>
+    </div>
   );
 };
 
