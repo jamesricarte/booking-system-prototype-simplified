5a13ecf18af7972ce05bd67cb4b5b81b5aaa1e52
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-05 17:14:22 +0800

    Finish the button and input

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 
 const Dashboard = () => {
   return (
     <main className="container w-full h-full px-4 mx-auto bg-white">
-      <Link>Today's Schedule Bookings</Link>
-      <Link>Available Rooms</Link>
-      <Link>Weekly Overview</Link>
-      <Link>Quick Actions Shortcut</Link>
+      <div className="mb-96">
+        <Link>Today's Schedule Bookings</Link>
+        <Link>Available Rooms</Link>
+        <Link>Weekly Overview</Link>
+        <Link>Quick Actions Shortcut</Link>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <Button name="login">Login</Button>
+        <Button name="signup">Sign Up</Button>
+        <Input type="text" name="email" placeholder="Enter your email">
+          Enter your email
+        </Input>
+      </div>
     </main>
   );
 };
