53e4b912b3b0c0770b231caae1b3ffc8f28ce174
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-08 16:50:38 +0800

    Fix paddings

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -10,16 +10,16 @@ const Dashboard = () => {
       <hr />
       <div className="pt-7 px-14">
         <div className="grid grid-cols-2 gap-8">
-          <Link className="shadow-md p-40 bg-[#F5F5F5] flex items-center justify-center rounded-md text-[27px] text-center">
+          <Link className="shadow-md p-36 bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px] text-center">
             Today's Schedule Bookings
           </Link>
-          <Link className="shadow-md p-40 bg-[#F5F5F5] flex items-center justify-center rounded-md text-[27px]">
+          <Link className="shadow-md p-36 bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px]">
             Available Rooms
           </Link>
-          <Link className="shadow-md p-40  bg-[#F5F5F5] flex items-center justify-center rounded-md text-[27px]">
+          <Link className="shadow-md p-36  bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px]">
             Weekly Overview
           </Link>
-          <Link className="shadow-md p-40  bg-[#F5F5F5] flex items-center justify-center rounded-md text-[27px]">
+          <Link className="shadow-md  p-36  bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px] text-center">
             Quick Actions Shortcut
           </Link>
         </div>
