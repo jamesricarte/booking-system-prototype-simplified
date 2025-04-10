d65538a39912a41e2499942f678ee8e312a4de4e
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-04-10 22:21:50 +0800

    Rename Dashboard component to AdminDashboard and wrap in DashboardLayout

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,30 +1,32 @@
-import { Link } from 'react-router-dom';
+import DashboardLayout from '../../layouts/DasboardLayout/DashboardLayout';
 
-const Dashboard = () => {
+const AdminDashboard = () => {
   return (
-    <main className="container w-full h-full bg-white">
-      <div className="p-4">
-        <h1 className="text-xl">Dashboard Section</h1>
-      </div>
-      <hr />
-      <div className="pt-7 px-14">
-        <div className="grid grid-cols-2 gap-8">
-          <Link className="shadow-md p-36 bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px] text-center">
-            System Summary
-          </Link>
-          <Link className="shadow-md p-36 bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px]">
-            Latest Booking Activities
-          </Link>
-          <Link className="shadow-md p-36  bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px]">
-            Upcoming Schedules
-          </Link>
-          <Link className="shadow-md  p-36  bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px] text-center">
-            Reports
-          </Link>
+    <DashboardLayout>
+      <main className="container w-full h-full bg-white">
+        <div className="p-4">
+          <h1 className="text-xl">Dashboard Section</h1>
         </div>
-      </div>
-    </main>
+        <hr />
+        <div className="pt-7 px-14">
+          <div className="grid grid-cols-2 gap-8">
+            <div className="shadow-md p-36 bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px] text-center">
+              System Summary
+            </div>
+            <div className="shadow-md p-36 bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px]">
+              Latest Booking Activities
+            </div>
+            <div className="shadow-md p-36 bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px]">
+              Upcoming Schedules
+            </div>
+            <div className="shadow-md p-36 bg-[#F5F5F5] flex items-center justify-center rounded-lg text-[27px] text-center">
+              Reports
+            </div>
+          </div>
+        </div>
+      </main>
+    </DashboardLayout>
   );
 };
 
-export default Dashboard;
+export default AdminDashboard;
