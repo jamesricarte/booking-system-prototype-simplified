926fc7c64b893a0820a910c279df0eb8e53ab2fd
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-04-10 23:46:17 +0800

    Refactor Admin section: remove AdminDashboard component, add HistoryOfOccupancy, Rooms, and Users components, and update routing in App.jsx

---
+++ client/src/pages/AdminDashboard/Users/users.jsx
@@ -0,0 +1,14 @@
+import Nav from '../../../components/Nav';
+
+const Users = () => {
+  return (
+    <>
+      <Nav />
+      <main className="flex flex-col items-center">
+        <h3>Users</h3>
+      </main>
+    </>
+  );
+};
+
+export default Users;
