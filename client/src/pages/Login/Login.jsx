e20c451da75f58bbe47a630d7a086e7cd8ccd89c
James Ricarte <uhenyou@gmail.com>
2025-04-30 06:45:32 +0800

    Added document titles for each page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -94,124 +94,130 @@ const Login = () => {
   };
 
   return (
-    <main className="flex">
-      <div className="w-[63vw] min-h-screen bg-white">
-        <img
-          src={BackGroundBu}
-          alt=""
-          className="object-cover w-full h-full opacity-90"
-        />
-      </div>
+    <>
+      <main className="flex">
+        <div className="w-[63vw] min-h-screen bg-white">
+          <img
+            src={BackGroundBu}
+            alt=""
+            className="object-cover w-full h-full opacity-90"
+          />
+        </div>
 
-      <div className="flex flex-col flex-grow h-screen px-4 py-4 overflow-y-auto">
-        <div className="my-auto">
-          <div className="flex flex-col items-center gap-2 px-8">
-            <img src={Logo} alt="Bicol University Logo" className="w-[80px]" />
-            <h2 className="text-[22px]">
-              Bicol University College of Engineering
-            </h2>
-          </div>
-          <form className="flex flex-col py-6 px-13" onSubmit={loginUser}>
-            <h1 className="mb-5 text-xl">Login</h1>
-            <div className="flex flex-col gap-4 mb-3">
-              <label htmlFor="email">Email Address</label>
-              <Input
-                type="text"
-                id="email"
-                name="email"
-                value={user.email}
-                onChange={handleUserInput}
-                required={true}
+        <div className="flex flex-col flex-grow h-screen px-4 py-4 overflow-y-auto">
+          <div className="my-auto">
+            <div className="flex flex-col items-center gap-2 px-8">
+              <img
+                src={Logo}
+                alt="Bicol University Logo"
+                className="w-[80px]"
               />
+              <h2 className="text-[22px]">
+                Bicol University College of Engineering
+              </h2>
             </div>
-            <div className="flex flex-col gap-4 mb-3">
-              <label htmlFor="password">Password</label>
-              <div className="relative w-full">
+            <form className="flex flex-col py-6 px-13" onSubmit={loginUser}>
+              <h1 className="mb-5 text-xl">Login</h1>
+              <div className="flex flex-col gap-4 mb-3">
+                <label htmlFor="email">Email Address</label>
                 <Input
-                  additionalClassName="w-full"
-                  type={showPassword ? "text" : "password"}
-                  id="password"
-                  name="password"
-                  value={user.password}
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={user.email}
                   onChange={handleUserInput}
                   required={true}
                 />
+              </div>
+              <div className="flex flex-col gap-4 mb-3">
+                <label htmlFor="password">Password</label>
+                <div className="relative w-full">
+                  <Input
+                    additionalClassName="w-full"
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    name="password"
+                    value={user.password}
+                    onChange={handleUserInput}
+                    required={true}
+                  />
 
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="absolute inset-y-0 flex items-center text-gray-600 cursor-pointer right-6"
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute inset-y-0 flex items-center text-gray-600 cursor-pointer right-6"
+                  >
+                    {showPassword ? (
+                      <HiMiniEyeSlash size={18} />
+                    ) : (
+                      <HiMiniEyeSlash size={18} color="#A9ADAB" />
+                    )}
+                  </button>
+                </div>
+              </div>
+
+              {response.isResponseAvailable && (
+                <p
+                  className={`mb-4 ${
+                    response.type === "success"
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }`}
                 >
-                  {showPassword ? (
-                    <HiMiniEyeSlash size={18} />
-                  ) : (
-                    <HiMiniEyeSlash size={18} color="#A9ADAB" />
-                  )}
-                </button>
+                  {response.message}
+                </p>
+              )}
+              <div className="flex items-center justify-between mb-5">
+                <div className="flex items-center gap-2">
+                  <input
+                    id="rememberMe"
+                    type="checkbox"
+                    className="w-3 h-3 cursor-pointer"
+                  />
+                  <label htmlFor="rememberMe">Remember me</label>
+                </div>
+                <Link
+                  to="/accountRecovery"
+                  className="text-[#FFA726] hover:underline"
+                >
+                  Forgot password?
+                </Link>
               </div>
-            </div>
-
-            {response.isResponseAvailable && (
-              <p
-                className={`mb-4 ${
-                  response.type === "success"
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {response.message}
-              </p>
-            )}
-            <div className="flex items-center justify-between mb-5">
-              <div className="flex items-center gap-2">
-                <input
-                  id="rememberMe"
-                  type="checkbox"
-                  className="w-3 h-3 cursor-pointer"
-                />
-                <label htmlFor="rememberMe">Remember me</label>
-              </div>
-              <Link
-                to="/accountRecovery"
-                className="text-[#FFA726] hover:underline"
-              >
-                Forgot password?
+              <Input
+                type="submit"
+                value="Login"
+                className="mb-2 bg-[#B3E5FC] p-4 rounded-md cursor-pointer"
+              />
+            </form>
+            <p className="mb-4 text-center">
+              Don't have an account?{" "}
+              <Link to="/register" className="text-[#FFA726]">
+                Register
               </Link>
-            </div>
-            <Input
-              type="submit"
-              value="Login"
-              className="mb-2 bg-[#B3E5FC] p-4 rounded-md cursor-pointer"
-            />
-          </form>
-          <p className="mb-4 text-center">
-            Don't have an account?{" "}
-            <Link to="/register" className="text-[#FFA726]">
-              Register
-            </Link>
-          </p>
-          <div className="mt-12">
-            <p className="text-sm text-center">
-              © 2025 BUCENG | All Rights Reserved{" "}
             </p>
+            <div className="mt-12">
+              <p className="text-sm text-center">
+                © 2025 BUCENG | All Rights Reserved{" "}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Loading spinner */}
-      <div
-        className={`absolute z-10 w-8 h-8 transform -translate-x-1/2 -translate-y-1/2 rounded-full border-6 rounded-1/2 border-t-transparent border-cyan-500 left-1/2 top-1/2 ${
-          loading ? "block animate-spin" : "hidden"
-        }`}
-      ></div>
+        {/* Loading spinner */}
+        <div
+          className={`absolute z-10 w-8 h-8 transform -translate-x-1/2 -translate-y-1/2 rounded-full border-6 rounded-1/2 border-t-transparent border-cyan-500 left-1/2 top-1/2 ${
+            loading ? "block animate-spin" : "hidden"
+          }`}
+        ></div>
 
-      {/* Background */}
-      <div
-        className={`fixed top-0 left-0 w-full h-full bg-white opacity-60 pointer-events-auto ${
-          loading ? "block" : "hidden"
-        }`}
-      ></div>
-    </main>
+        {/* Background */}
+        <div
+          className={`fixed top-0 left-0 w-full h-full bg-white opacity-60 pointer-events-auto ${
+            loading ? "block" : "hidden"
+          }`}
+        ></div>
+      </main>
+    </>
   );
 };
 
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -88,156 +88,162 @@ const Register = () => {
   };
 
   return (
-    <main className="flex">
-      <div className="w-[63vw] min-h-screen bg-white">
-        <img
-          src={BackGroundBu}
-          alt=""
-          className="object-cover w-full h-full opacity-90"
-        />
-      </div>
+    <>
+      <main className="flex">
+        <div className="w-[63vw] min-h-screen bg-white">
+          <img
+            src={BackGroundBu}
+            alt=""
+            className="object-cover w-full h-full opacity-90"
+          />
+        </div>
 
-      <div className="flex flex-col flex-grow h-screen px-4 py-12 overflow-y-auto">
-        <div className="my-auto">
-          <div className="flex flex-col items-center gap-2 px-8">
-            <img src={Logo} alt="Bicol University Logo" className="w-[80px]" />
-            <h2 className="text-[22px]">
-              Bicol University College of Engineering
-            </h2>
-          </div>
-          <form className="flex flex-col p-6 px-13" onSubmit={registerUser}>
-            <h1 className="mb-5 text-xl">Sign Up</h1>
-            <div className="flex flex-col gap-4 mb-3">
-              <label htmlFor="email">
-                Email Address
-                <span className="text-[#F56C18] font-bold">*</span>
-              </label>
-              <Input
-                type="text"
-                id="email"
-                name="email"
-                value={user.email}
-                onChange={handleUserInput}
-                required={true}
+        <div className="flex flex-col flex-grow h-screen px-4 py-12 overflow-y-auto">
+          <div className="my-auto">
+            <div className="flex flex-col items-center gap-2 px-8">
+              <img
+                src={Logo}
+                alt="Bicol University Logo"
+                className="w-[80px]"
               />
+              <h2 className="text-[22px]">
+                Bicol University College of Engineering
+              </h2>
             </div>
-            <div className="flex flex-col gap-4 mb-3">
-              <label htmlFor="schoolId">
-                School Id<span className="text-[#F56C18] font-bold">*</span>
-              </label>
-              <Input
-                type="text"
-                id="schoolId"
-                name="schoolId"
-                value={user.schoolId}
-                onChange={handleUserInput}
-                required={true}
-              />
-            </div>
-            <div className="flex flex-col gap-4 mb-3">
-              <label htmlFor="password">
-                Password<span className="text-[#F56C18] font-bold">*</span>
-              </label>
-              <div className="relative w-full">
+            <form className="flex flex-col p-6 px-13" onSubmit={registerUser}>
+              <h1 className="mb-5 text-xl">Sign Up</h1>
+              <div className="flex flex-col gap-4 mb-3">
+                <label htmlFor="email">
+                  Email Address
+                  <span className="text-[#F56C18] font-bold">*</span>
+                </label>
                 <Input
-                  additionalClassName="w-full"
-                  type={showPassword ? "text" : "password"}
-                  id="password"
-                  name="password"
-                  value={user.password}
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={user.email}
                   onChange={handleUserInput}
                   required={true}
                 />
-
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="absolute inset-y-0 flex items-center text-gray-600 cursor-pointer right-6"
-                >
-                  {showPassword ? (
-                    <HiMiniEyeSlash size={18} />
-                  ) : (
-                    <HiMiniEyeSlash size={18} color="#A9ADAB" />
-                  )}
-                </button>
               </div>
-            </div>
-            <div className="flex flex-col gap-4 mb-7">
-              <label htmlFor="confirmPassword">
-                Confirm Password
-                <span className="text-[#F56C18] font-bold">*</span>
-              </label>
-              <div className="relative w-full">
+              <div className="flex flex-col gap-4 mb-3">
+                <label htmlFor="schoolId">
+                  School Id<span className="text-[#F56C18] font-bold">*</span>
+                </label>
                 <Input
-                  additionalClassName="w-full"
-                  type={showConfirmPassword ? "text" : "password"}
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  value={user.confirmPassword}
+                  type="text"
+                  id="schoolId"
+                  name="schoolId"
+                  value={user.schoolId}
                   onChange={handleUserInput}
                   required={true}
                 />
-
-                <button
-                  type="button"
-                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                  className="absolute inset-y-0 flex items-center text-gray-600 cursor-pointer right-6"
-                >
-                  {showConfirmPassword ? (
-                    <HiMiniEyeSlash size={18} />
-                  ) : (
-                    <HiMiniEyeSlash size={18} color="#A9ADAB" />
-                  )}
-                </button>
               </div>
-            </div>
+              <div className="flex flex-col gap-4 mb-3">
+                <label htmlFor="password">
+                  Password<span className="text-[#F56C18] font-bold">*</span>
+                </label>
+                <div className="relative w-full">
+                  <Input
+                    additionalClassName="w-full"
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    name="password"
+                    value={user.password}
+                    onChange={handleUserInput}
+                    required={true}
+                  />
 
-            {response.isResponseAvailable && (
-              <p
-                className={`mb-4 ${
-                  response.type === "success"
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {response.message}
-                {response.type === "success" && "... Redirecting to login"}
-              </p>
-            )}
-            <Input
-              type="submit"
-              value="Register"
-              className=" bg-[#B3E5FC] p-4 rounded-md cursor-pointer"
-            />
-          </form>
-          <p className="mb-4 text-center">
-            Already have an account?{" "}
-            <Link to="/login" className="text-[#FFA726]">
-              Login
-            </Link>
-          </p>
-          <div className="mt-12">
-            <p className="text-sm text-center">
-              © 2025 BUCENG | All Rights Reserved{" "}
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute inset-y-0 flex items-center text-gray-600 cursor-pointer right-6"
+                  >
+                    {showPassword ? (
+                      <HiMiniEyeSlash size={18} />
+                    ) : (
+                      <HiMiniEyeSlash size={18} color="#A9ADAB" />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 mb-7">
+                <label htmlFor="confirmPassword">
+                  Confirm Password
+                  <span className="text-[#F56C18] font-bold">*</span>
+                </label>
+                <div className="relative w-full">
+                  <Input
+                    additionalClassName="w-full"
+                    type={showConfirmPassword ? "text" : "password"}
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    value={user.confirmPassword}
+                    onChange={handleUserInput}
+                    required={true}
+                  />
+
+                  <button
+                    type="button"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                    className="absolute inset-y-0 flex items-center text-gray-600 cursor-pointer right-6"
+                  >
+                    {showConfirmPassword ? (
+                      <HiMiniEyeSlash size={18} />
+                    ) : (
+                      <HiMiniEyeSlash size={18} color="#A9ADAB" />
+                    )}
+                  </button>
+                </div>
+              </div>
+
+              {response.isResponseAvailable && (
+                <p
+                  className={`mb-4 ${
+                    response.type === "success"
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }`}
+                >
+                  {response.message}
+                  {response.type === "success" && "... Redirecting to login"}
+                </p>
+              )}
+              <Input
+                type="submit"
+                value="Register"
+                className=" bg-[#B3E5FC] p-4 rounded-md cursor-pointer"
+              />
+            </form>
+            <p className="mb-4 text-center">
+              Already have an account?{" "}
+              <Link to="/login" className="text-[#FFA726]">
+                Login
+              </Link>
             </p>
+            <div className="mt-12">
+              <p className="text-sm text-center">
+                © 2025 BUCENG | All Rights Reserved{" "}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Loading spinner */}
-      <div
-        className={`absolute z-10 w-8 h-8 transform -translate-x-1/2 -translate-y-1/2 rounded-full border-6 rounded-1/2 border-t-transparent border-cyan-500 left-1/2 top-1/2 ${
-          loading ? "block animate-spin" : "hidden"
-        }`}
-      ></div>
+        {/* Loading spinner */}
+        <div
+          className={`absolute z-10 w-8 h-8 transform -translate-x-1/2 -translate-y-1/2 rounded-full border-6 rounded-1/2 border-t-transparent border-cyan-500 left-1/2 top-1/2 ${
+            loading ? "block animate-spin" : "hidden"
+          }`}
+        ></div>
 
-      {/* Background */}
-      <div
-        className={`fixed top-0 left-0 w-full h-full bg-white opacity-60 pointer-events-auto ${
-          loading ? "block" : "hidden"
-        }`}
-      ></div>
-    </main>
+        {/* Background */}
+        <div
+          className={`fixed top-0 left-0 w-full h-full bg-white opacity-60 pointer-events-auto ${
+            loading ? "block" : "hidden"
+          }`}
+        ></div>
+      </main>
+    </>
   );
 };
 
